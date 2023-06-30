e4f42925cc0d786b89c821dcf799baa63c130f6d
polishukova <pollimagenta@gmail.com>
2023-06-30 12:32:16 +0200

    feat(graduate-work): try to make search with redux thunk

--- src/Components/Films/getFilms.ts
+++ src/Components/Films/getFilms.ts
@@ -1,6 +1,7 @@
-const FILMS = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
-const GENRES = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
-const FINDBYID = 'https://api.themoviedb.org/3/movie/'
+const FILMS = 'https://api.themoviedb.org/3/trending/movie/day';
+const GENRES = 'https://api.themoviedb.org/3/genre/movie/list';
+const FINDBYID = 'https://api.themoviedb.org/3/movie'
+const FINDBYSEARCH = 'https://api.themoviedb.org/3/search/movie'
 
 export type OneFilm = {
     genre_ids: number[],
@@ -10,7 +11,7 @@ export type OneFilm = {
     vote_average: number,
 };
 
-export type OneFilmWithGenre = OneFilm & {genre: (string | undefined) []};
+export type OneFilmWithGenre = OneFilm & { genre: (string | undefined)[] };
 
 export type Response = {
     page: number,
@@ -19,10 +20,9 @@ export type Response = {
     total_results: number,
 };
 
-export const getFilms = async (page?: number, search?: string) => {
+export const getFilms = async (page?: number) => {
     const filmsUrl = new URL(FILMS);
     if (page) filmsUrl.searchParams.set("page", String(page));
-    if (search) filmsUrl.searchParams.set("search", String(search))
     const options = {
         method: 'GET',
         headers: {
@@ -36,26 +36,26 @@ export const getFilms = async (page?: number, search?: string) => {
 };
 
 export type FilmForSinglePage = {
-budget: number,
-genres: OneGenre[],
-id: number,
-overview: string,
-poster_path: string,
-production_companies: ProductionCompanies[],
-production_countries: ProductionCountries[],
-release_date: string, 
-runtime: number,
-tagline: string,
-title: string,
-vote_average: number,
+    budget: number,
+    genres: OneGenre[],
+    id: number,
+    overview: string,
+    poster_path: string,
+    production_companies: ProductionCompanies[],
+    production_countries: ProductionCountries[],
+    release_date: string,
+    runtime: number,
+    tagline: string,
+    title: string,
+    vote_average: number,
 }
 
-export type ProductionCompanies = {name: string}
+export type ProductionCompanies = { name: string }
 
-export type ProductionCountries = {name: string}
+export type ProductionCountries = { name: string }
 
 export const getFilm = async (filmId: string) => {
-    const filmsUrl = new URL(FINDBYID + filmId);
+    const filmsUrl = new URL(FINDBYID + '/' + filmId);
     const options = {
         method: 'GET',
         headers: {
@@ -66,7 +66,7 @@ export const getFilm = async (filmId: string) => {
     const response = await fetch(filmsUrl, options);
     const result: FilmForSinglePage = await response.json();
     console.log("🚀 ~ file: getFilms.ts:48 ~ getFilm ~ result:", result)
-    
+
     return result
 }
 
@@ -76,7 +76,7 @@ export type OneGenre = {
 }
 
 export type Genres = {
-    genres: OneGenre [],
+    genres: OneGenre[],
 }
 
 export const getGenres = async () => {
@@ -92,4 +92,22 @@ export const getGenres = async () => {
     const response = await fetch(genresUrl, options);
     const result: Genres = await response.json();
     return result.genres
+}
+
+type SearchParams = { search?: string }
+
+export const getFilmsBySearch = async ({ search }: SearchParams) => {
+    const filmsUrl = new URL(FINDBYSEARCH);
+    filmsUrl.searchParams.set("search", String(search));
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzc1ZDIyMDYyMDY2NGFkZjM5MGZjYTU1NjIwMDA0ZiIsInN1YiI6IjY0N2M3NjVlMGUyOWEyMDExNmFkNDU2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ihZhctfHnZABpjMVWFHZMMvNEZG6rMdJ5plj0SAbsag'
+        }
+    };
+
+    const response = await fetch(filmsUrl, options);
+    const result: Response = await response.json();
+    return result
 }
--- src/Store/films.ts
+++ src/Store/films.ts
@@ -1,5 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { OneFilm } from "../Components/Films/getFilms"
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { getFilmsBySearch, OneFilm } from "../Components/Films/getFilms"
+import { RootState, store } from "./store"
 
 const initialState: { films: OneFilm[]} = { films: []}
 
@@ -10,8 +11,19 @@ export const filmsSlice = createSlice({
         setFilms: (state, action: PayloadAction<OneFilm[]>) => {
             state.films = action.payload
         },
-    }
+    },
+    extraReducers(builder) {
+        builder.addCase(getFilmsThunk.fulfilled, (state, action: PayloadAction<OneFilm[]>) => {
+            state.films = action.payload
+        })
+    },
 })
 
 export const { setFilms } = filmsSlice.actions
 export const filmsReducer = filmsSlice.reducer
+
+export const getFilmsThunk = createAsyncThunk<OneFilm[], {search?:string}, {state: RootState}>('films/getFilms', async ({search = ''}, store) => {
+    const state = store.getState()
+    const films = await getFilmsBySearch({'search': search})
+    return films
+})
